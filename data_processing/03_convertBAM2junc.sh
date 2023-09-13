cae4cd0cb15ccb02af977f2f02e8d21232437a00
dmanahan1 <89279066+dmanahan1@users.noreply.github.com>
2023-09-13 11:12:52 -0700

    Update 03_convertBAM2junc.sh
    Reword comment explaining regtools

--- data_processing/03_convertBAM2junc.sh
+++ data_processing/03_convertBAM2junc.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 ## Converts bam files to junc files.  junc files are human readable and only contain exon-exon junctions from the RNA-Seq BAM file.
-## For more information, including column content: https://regtools.readthedocs.io/en/latest/commands/junctions-extract/
+## For more information, e.g. understanding what data is in each column: https://regtools.readthedocs.io/en/latest/commands/junctions-extract/
 
-# loop through all bam files in the current directory
+# Loop through all bam files in the current directory.
 for bamfile in `ls *bam`; do
 
-    # print a message indicating which file is being converted
+    # Print a message indicating which file is being converted
     echo Converting $bamfile to $bamfile.junc
 
-    # index the bam file using samtools
+    # Index the bam file using samtools
     samtools index $bamfile
 
-    # extract junctions using regtools
+    # Extract junctions using regtools
     regtools junctions extract -s 0 -a 8 -m 50 -M 500000 $bamfile -o $bamfile.junc
 
-    # add the path to the junction file to a text file
+    # Add the path to the junction file to a text file
     echo $bamfile.junc >> population_juncfiles.txt
 
 done
