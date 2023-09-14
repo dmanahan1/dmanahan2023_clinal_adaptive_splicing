cbb614cf0ec7c5d7771cbefebdb9983f0df86fe4
dmanahan1 <89279066+dmanahan1@users.noreply.github.com>
2023-09-14 15:28:25 -0700

    Create leafcutter_mg39annotation.sh

---
+++ build_references/leafcutter_mg39annotation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+## Creates a directory containing relevant annotation (e.g. annoated splice junctions) build from a gtf
+
+/path/to/leafcutter/leafviz/gtf2leafcutter.pl -o mg39_annotation /path/to/mg39_index/gencode.vM29.annotation.gtf
